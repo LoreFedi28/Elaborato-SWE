0c2d8934fba8ee98a7ad2031fc5df8de50cb73a6
lorenzofedi <fedilorenzogm@gmail.com>
2025-03-05 11:08:55 +0100

    implementazione classi Doctor e Person

--- Gestionale Visite Mediche/src/main/java/domainModel/Doctor.java
+++ Gestionale Visite Mediche/src/main/java/domainModel/Doctor.java
@@ -1,4 +1,30 @@
 package main.java.domainModel;
 
-public class Doctor {
+public class Doctor extends Person{
+
+    private String iban;
+
+    public Doctor(String doctorCF, String name, String surname, String iban){
+        super(doctorCF, name, surname);
+        this.iban = iban;
+    }
+
+    public String getIban(){
+        return iban;
+    }
+
+    public void setIban(String iban){
+        this.iban = iban;
+    }
+
+    @Override
+    public String toString(){
+        return "Doctor{" +
+                " codice fiscale =' " + getCF() + '\'' +
+                ", name = '" + getName() + '\'' +
+                ", surname = '" + getSurname() + '\'' +
+                ", iban = " + iban +
+                '}';
+    }
+
 }
