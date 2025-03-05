f9b7f695ff11ffd6b8c474343c0088b3ca3ef856
lorenzofedi <fedilorenzogm@gmail.com>
2025-03-05 10:55:46 +0100

    implementazione classe Patient

--- Gestionale Visite Mediche/src/main/java/domainModel/Patient.java
+++ Gestionale Visite Mediche/src/main/java/domainModel/Patient.java
@@ -1,4 +1,29 @@
 package main.java.domainModel;
 
-public class Patient {
+public class Patient extends Person {
+
+    private String level;
+
+    public Patient(String patientCF, String name, String surname, String level){
+        super(patientCF, name, surname);
+        this.level = level;
+    }
+
+    public String getLevel(){
+        return level;
+    }
+
+    public void setLevel(String level){
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "Trainer{" +
+                "fiscalCode='" + getCF() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", surname='" + getSurname() + '\'' +
+                ", level=" + level +
+                '}';
+    }
 }
